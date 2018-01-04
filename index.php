5841f8e9c21ae2ac78e4cf83063df446fded5ed1
carvaalholeo <leonardocarvalho74@gmail.com>
2018-01-04 14:51:15 -0200

    Listando usuários

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ $app = new Slim();
 
 $app->config('debug', true);
 
+// Tela principal
 $app->get('/', function() {
 
     $page = new Page();
@@ -18,7 +19,7 @@ $app->get('/', function() {
     $page->setTpl("index");
 
 });
-
+// Tela de admin
 $app->get('/admin', function() {
 
     User::verifyLogin();
@@ -28,7 +29,7 @@ $app->get('/admin', function() {
     $page->setTpl("index");
 
 });
-
+// Tela de login
 $app->get('/admin/login', function (){
 
     $page = new PageAdmin([
@@ -39,6 +40,7 @@ $app->get('/admin/login', function (){
     $page->setTpl("login");
 });
 
+// Rota para tela de login caso tente entrar sem logar
 $app->post('/admin/login', function(){
 
     User::login($_POST["login"], $_POST["password"]);
@@ -47,7 +49,7 @@ $app->post('/admin/login', function(){
     exit;
 
 });
-
+// Rota de logout tela admin
 $app->get('/admin/logout', function (){
 
     User::logout();
@@ -56,6 +58,76 @@ $app->get('/admin/logout', function (){
     exit;
 
 });
+// Rota para listar usuário
+$app->get("/admin/users", function (){
+
+    User::verifyLogin();
+
+    $users = User::listAll();
+
+    $page = new PageAdmin();
+
+    $page->setTpl("users", array(
+        "users"=>$users
+    ));
+
+});
+
+// Rota para criar usuário
+$app->get("/admin/users/create", function (){
+
+    User::verifyLogin();
+
+    $page = new PageAdmin();
+
+    $page->setTpl("users-create");
+
+});
+
+// Deletar o usuário
+$app->get("/admin/users/:iduser/delete", function ($iduser){
+
+    User::verifyLogin();
+
+});
+
+// Rota para editar usuário
+$app->get("/admin/users/:iduser", function ($iduser){
+
+    User::verifyLogin();
+
+    $page = new PageAdmin();
+
+    $page->setTpl("users-update");
+
+});
+
+// Salvar a criação do usuário
+$app->post("/admin/users/create", function (){
+
+    User::verifyLogin();
+
+    $user = new User();
+
+    $_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+    $user->setData($_POST);
+
+    $user->save();
+
+    header("Location: /admin/users");
+    exit;
+
+});
+
+// Salvar a edição do usuário
+$app->post("/admin/users/:iduser", function ($iduser){
+
+    User::verifyLogin();
+
+});
+
+
 
 $app->run();
 
